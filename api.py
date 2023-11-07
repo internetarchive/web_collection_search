09cad27c18d72d0e83ec665cb0735757a79bbe76
Paige Gulley <pgulley@ludwig.local>
2023-11-07 13:38:43 -0500

    pythonicality

--- api.py
+++ api.py
@@ -41,7 +41,7 @@ ES = Elasticsearch(config["eshosts"], **config["esopts"])
 
 def get_allowed_collections():
     #Only expose indexes with the correct prefix, and add a wildcard as well. 
-    all_indexes = [index for index in ES.indices.get(index='*') if ELASTICSEARCH_INDEX_NAME_PREFIX in index]
+    all_indexes = [index for index in ES.indices.get(index='*') if index.startswith(ELASTICSEARCH_INDEX_NAME_PREFIX)]
     all_indexes.extend([f"{ELASTICSEARCH_INDEX_NAME_PREFIX}_*)"])
     return all_indexes
 
